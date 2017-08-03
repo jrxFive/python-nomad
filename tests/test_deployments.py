d8041bfd63bddb79f09c38eefac8a3036d5c3e96
Jonathan Cross <jonathan.cross@barkly.com>
2017-08-03 18:39:18 -0400

    add skip decorators for deployments tests that talk to api

--- tests/test_deployments.py
+++ tests/test_deployments.py
@@ -15,31 +15,31 @@ def nomad_setup():
 def test_get_evaluation(nomad_setup):
     assert "example" == nomad_setup.deployments.get_deployments()[0]["JobID"]
 
-
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 6, 0), reason="Not supported in version")
 def test_dunder_getitem_exist(nomad_setup):
     jobID = nomad_setup.deployments.get_deployments()[0]["ID"]
     d = nomad_setup.deployment[jobID]
     assert isinstance(d, dict)
 
-
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 6, 0), reason="Not supported in version")
 def test_dunder_getitem_not_exist(nomad_setup):
     with pytest.raises(KeyError):
         _ = nomad_setup.deployments["nope"]
 
-
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 6, 0), reason="Not supported in version")
 def test_dunder_contain_exists(nomad_setup):
     jobID = nomad_setup.deployments.get_deployments()[0]["ID"]
     assert jobID in nomad_setup.deployments
 
-
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 6, 0), reason="Not supported in version")
 def test_dunder_contain_not_exist(nomad_setup):
     assert "nope" not in nomad_setup.deployments
 
-
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 6, 0), reason="Not supported in version")
 def test_dunder_len(nomad_setup):
     assert len(nomad_setup.deployments) == 1
 
-
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 6, 0), reason="Not supported in version")
 def test_dunder_iter(nomad_setup):
     for d in nomad_setup.deployments:
         pass
