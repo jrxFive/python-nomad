658ca63c68924e72978cab2504b65cb6cebb9579
Pedro.Serrano.contractor <pedro.serrano@i4s.com>
2017-12-30 20:13:16 +0100

    remove version from environment for test

--- tests/common.py
+++ tests/common.py
@@ -6,9 +6,6 @@ URI = os.environ.get("NOMAD_URL", "http://192.168.33.10")
 # internal ip of docker
 IP = os.environ.get("NOMAD_IP", "192.168.33.10")
 
-# Security token
-NOMAD_VERSION = os.environ.get("NOMAD_VERSION", "0.7.1")
-
 # use vagrant PORT if env variable is not specified, generally for local
 # testing
 NOMAD_PORT = os.environ.get("NOMAD_PORT", 4646)
