e81ec1d09deabdb49f0dee84ca41ae7accaf972d
Pedro.Serrano.contractor <pedro.serrano@i4s.com>
2018-01-01 18:28:06 +0100

    addapt test cases for setup versions - ACL -

--- tests/test_acl.py
+++ tests/test_acl.py
@@ -15,16 +15,19 @@ def nomad_setup():
 # integration tests requires nomad Vagrant VM or Binary running
 # IMPORTANT: without token activated
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 @pytest.mark.run(order=0)
 def test_create_bootstrap(nomad_setup):
     bootstrap = nomad_setup.acl.generate_bootstrap()
     assert "SecretID" in bootstrap
     common.NOMAD_TOKEN = bootstrap["SecretID"]
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 @pytest.mark.run(order=1)
 def test_list_tokens(nomad_setup):
     assert "Bootstrap Token" in nomad_setup.acl.get_tokens()[0]["Name"]
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 @pytest.mark.run(order=2)
 def test_create_token(nomad_setup):
     token_example='{"Name": "Readonly token","Type": "client","Policies": ["readonly"],"Global": false}'
@@ -32,11 +35,13 @@ def test_create_token(nomad_setup):
     created_token = nomad_setup.acl.create_token(json_token)
     assert "Readonly token" in created_token["Name"]
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 @pytest.mark.run(order=3)
 def test_list_all_tokens(nomad_setup):
     tokens = nomad_setup.acl.get_tokens()
     assert isinstance(tokens, list)
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 @pytest.mark.run(order=4)
 def test_update_token(nomad_setup):
     token_example='{"Name": "CreatedForUpdate","Type": "client","Policies": ["readonly"],"Global": false}'
@@ -48,6 +53,7 @@ def test_update_token(nomad_setup):
     update_token = nomad_setup.acl.update_token(id=created_token["AccessorID"],token=json_token_update)
     assert "Updated" in update_token["Name"]
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 @pytest.mark.run(order=5)
 def test_get_token(nomad_setup):
     token_example='{"Name": "GetToken","Type": "client","Policies": ["readonly"],"Global": false}'
@@ -57,6 +63,7 @@ def test_get_token(nomad_setup):
     get_token = nomad_setup.acl.get_token(created_token["AccessorID"])
     assert "GetToken" in created_token["Name"]
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 @pytest.mark.run(order=6)
 def test_delete_token(nomad_setup):
     token_example='{"Name": "DeleteToken","Type": "client","Policies": ["readonly"],"Global": false}'
@@ -67,30 +74,36 @@ def test_delete_token(nomad_setup):
     nomad_setup.acl.delete_token(created_token["AccessorID"])
     assert False == any("DeleteToken" in x for x in nomad_setup.acl.get_tokens())
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 def test_get_selftoken(nomad_setup):
     get_token = nomad_setup.acl.get_selftoken()
     assert nomad_setup.token in get_token["SecretID"]
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 def test_get_policies(nomad_setup):
     policies = nomad_setup.acl.get_policies()
     assert isinstance(policies, list)
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 def test_create_policy(nomad_setup):
     policy_example = '{ "Name": "my-policy", "Description": "This is a great policy", "Rules": "" }'
     json_policy = json.loads(policy_example)
     nomad_setup.acl.create_policy(id="my-policy", policy=json_policy)
     assert False == any("my-policy" in x for x in nomad_setup.acl.get_policies())
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 def test_get_policy(nomad_setup):
     policy = nomad_setup.acl.get_policy("my-policy")
     assert "This is a great policy" in policy["Description"]
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 def test_update_policy(nomad_setup):
     policy_update = '{"Name": "my-policy","Description": "Updated","Rules": ""}'
     json_policy_update = json.loads(policy_update)
     nomad_setup.acl.update_policy(id="my-policy", policy=json_policy_update)
     assert False == any("Updated" in x for x in nomad_setup.acl.get_policies())
 
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 7, 0), reason="Nomad dispatch not supported")
 def test_delete_policy(nomad_setup):
     nomad_setup.acl.delete_policy(id="my-policy")
     assert False == any("my-policy" in x for x in nomad_setup.acl.get_policies())
--- tests/test_centinel.py
+++ tests/test_centinel.py
@@ -17,7 +17,7 @@ def nomad_setup():
 
 # integration tests was mocked.
 
-@patch('nomad.api.quotas.Quotas._get')
+@patch('nomad.api.sentinel.Sentinel._get')
 def test_list_policies(mock_get, nomad_setup):
     mock_get.return_value = [
                               {
@@ -30,25 +30,25 @@ def test_list_policies(mock_get, nomad_setup):
                                 "ModifyIndex": 8
                               }
                             ]
-    policies = nomad_setup.quotas.get_policies()
+    policies = nomad_setup.sentinel.get_policies()
     assert isinstance(policies, list)
-    assert "foo" in nomad_setup.quotas.get_policies()[0]["Name"]
+    assert "foo" in nomad_setup.sentinel.get_policies()[0]["Name"]
 
-@patch('nomad.api.quotas.Quotas._post_no_json')
+@patch('nomad.api.sentinel.Sentinel._post_no_json')
 def test_create_policy(mock_post_no_json, nomad_setup):
     mock_post_no_json.return_value = requests.codes.ok
     policy_example = '{"Name": "my-policy", "Description": "This is a great policy", "Scope": "submit-job", "EnforcementLevel": "advisory", "Policy": "main = rule { true }"}'
     json_policy = json.loads(policy_example)
-    assert 200 == nomad_setup.quotas.create_policy(id="my-policy", policy=json_policy)
+    assert 200 == nomad_setup.sentinel.create_policy(id="my-policy", policy=json_policy)
 
-@patch('nomad.api.quotas.Quotas._post_no_json')
+@patch('nomad.api.sentinel.Sentinel._post_no_json')
 def test_update_policy(mock_post_no_json, nomad_setup):
     mock_post_no_json.return_value = requests.codes.ok
     policy_example = '{"Name": "my-policy", "Description": "Update", "Scope": "submit-job", "EnforcementLevel": "advisory", "Policy": "main = rule { true }"}'
     json_policy = json.loads(policy_example)
-    assert 200 == nomad_setup.quotas.update_policy(id="my-policy", policy=json_policy)
+    assert 200 == nomad_setup.sentinel.update_policy(id="my-policy", policy=json_policy)
 
-@patch('nomad.api.quotas.Quotas._get')
+@patch('nomad.api.sentinel.Sentinel._get')
 def test_get_policy(mock_get, nomad_setup):
     mock_get.return_value = {
                               "Name": "foo",
@@ -60,11 +60,11 @@ def test_get_policy(mock_get, nomad_setup):
                               "CreateIndex": 8,
                               "ModifyIndex": 8
                             }
-    policy = nomad_setup.quotas.get_policy("foo")
+    policy = nomad_setup.sentinel.get_policy("foo")
     assert "advisory" in policy["EnforcementLevel"]
 
-@patch('nomad.api.quotas.Quotas._delete')
+@patch('nomad.api.sentinel.Sentinel._delete')
 def test_delete_policy(mock_delete, nomad_setup):
     mock_delete.return_value = requests.codes.ok
-    nomad_setup.quotas.delete_policy(id="my-policy")
-    assert 200 == nomad_setup.quotas.delete_policy(id="my-policy")
+    nomad_setup.sentinel.delete_policy(id="my-policy")
+    assert 200 == nomad_setup.sentinel.delete_policy(id="my-policy")
