7a751ff82cd3a32082f00e6985a0307db80f5c90
Jonathan Cross <jrxFive@gmail.com>
2018-01-10 17:01:08 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-nomad',
-    version='0.5.0',
+    version='0.6.0',
     install_requires=['requests'],
     packages=['nomad', 'nomad.api'],
     url='http://github.com/jrxfive/python-nomad',
