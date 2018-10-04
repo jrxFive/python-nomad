72d0796e96fc1a1df955d3975b86893ab71d2662
Pedro Serrano <pedro.serrano.next@bbva.com>
2018-10-04 09:27:47 +0200

    review job files at travis

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -109,11 +109,13 @@ fi
 echo "Nomad: Create test job samples"
 /usr/bin/nomad init
 /usr/bin/nomad run -output example.nomad > example.json
-if [[ ${MAJOR_VERSION} -gt 7 ]]; then
+if [[ ${MAJOR_VERSION} -ge 8 ]]; then
   cp example.nomad vault.hcl
   sed -i s/"# vault {"/"vault { policies = [\"policy-demo\"]}"/g vault.hcl
   sed -i s/"job \"example\" {"/"job \"vault\" {"/g vault.hcl
   /usr/bin/nomad run -output vault.hcl > vault.json
+  cat vault.hcl
+  cat vault.json
 fi
 
 
