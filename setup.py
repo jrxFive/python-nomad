c3eb38373c6d73cbc3be18a2d24e32c172c759c6
Jonathan Cross <jrxFive@gmail.com>
2018-11-09 11:34:59 -0500

    bump version for query string fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-nomad',
-    version='1.0.1',
+    version='1.0.2',
     install_requires=['requests'],
     packages=['nomad', 'nomad.api'],
     url='http://github.com/jrxfive/python-nomad',
