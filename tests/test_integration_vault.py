4be2646645392cbfb9f449e77b5b95eaeca3eb7f
Pedro Serrano <pedro.serrano.next@bbva.com>
2018-11-11 14:02:25 +0100

    review fix at nomad and vault integration to skip non valid test 0.8.5 and 0.5.6. From nomad changelog.md

--- tests/test_integration_vault.py
+++ tests/test_integration_vault.py
@@ -26,11 +26,14 @@ def requests_retry_session(retries=3, backoff_factor=0.3,
 # Register Job
 # Depens on version uses versioned secrets
 #
+# NOMAD v 0.8.5 --> vault: Fix a regression in which Nomad was only compatible with Vault versions greater than 0.10.0 [GH-4698]
+# NOMAD v 0.5.6 --> server/vault: Fix Vault Client panic when given nonexistent role [GH-2648]
 @pytest.mark.skipif(tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 6, 2)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,8,5)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) <= (0,5,0)
+    or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,5,6)
     or os.environ.get("VAULT_TEST") == "false",
-    reason="Not supported in version. At version 0.8.5 see regresion of 8.5.6 at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md, or you have configured a false VAULT_TEST")
+    reason="0.8.5 vault: Fix [GH-4698]. 0.5.6 server/vault: [GH-2648]. Review at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md")
 def test_register_vault_job_valid(nomad_setup_vault_valid_token):
     if tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 9, 0):
         vault_job = "vault.json"
@@ -47,11 +50,14 @@ def test_register_vault_job_valid(nomad_setup_vault_valid_token):
 # Get Job
 # Depens on version uses versioned secrets
 #
+# NOMAD v 0.8.5 --> vault: Fix a regression in which Nomad was only compatible with Vault versions greater than 0.10.0 [GH-4698]
+# NOMAD v 0.5.6 --> server/vault: Fix Vault Client panic when given nonexistent role [GH-2648]
 @pytest.mark.skipif(tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 6, 2)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,8,5)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) <= (0,5,0)
+    or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,5,6)
     or os.environ.get("VAULT_TEST") == "false",
-    reason="Not supported in version. At version 0.8.5 see regresion of 8.5.6 at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md, or you have configured a false VAULT_TEST")
+    reason="0.8.5 vault: Fix [GH-4698]. 0.5.6 server/vault: [GH-2648]. Review at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md")
 def test_get_vault_job_valid(nomad_setup_vault_valid_token):
     assert isinstance(nomad_setup_vault_valid_token.job.get_job("vault"), dict) == True
 
@@ -61,11 +67,14 @@ def test_get_vault_job_valid(nomad_setup_vault_valid_token):
 # Validate Job
 # Depens on version uses versioned secrets
 #
+# NOMAD v 0.8.5 --> vault: Fix a regression in which Nomad was only compatible with Vault versions greater than 0.10.0 [GH-4698]
+# NOMAD v 0.5.6 --> server/vault: Fix Vault Client panic when given nonexistent role [GH-2648]
 @pytest.mark.skipif(tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 6, 2)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,8,5)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) <= (0,5,0)
+    or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,5,6)
     or os.environ.get("VAULT_TEST") == "false",
-    reason="Not supported in version. At version 0.8.5 see regresion of 8.5.6 at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md, or you have configured a false VAULT_TEST")
+    reason="0.8.5 vault: Fix [GH-4698]. 0.5.6 server/vault: [GH-2648]. Review at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md")
 def test_get_vault_job_valid(nomad_setup_vault_valid_token):
     assert isinstance(nomad_setup_vault_valid_token.job.get_job("vault"), dict) == True
 
@@ -74,11 +83,14 @@ def test_get_vault_job_valid(nomad_setup_vault_valid_token):
 # Validate secret from vault
 # Depens on version uses versioned secrets
 #
+# NOMAD v 0.8.5 --> vault: Fix a regression in which Nomad was only compatible with Vault versions greater than 0.10.0 [GH-4698]
+# NOMAD v 0.5.6 --> server/vault: Fix Vault Client panic when given nonexistent role [GH-2648]
 @pytest.mark.skipif(tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 6, 2)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,8,5)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) <= (0,5,0)
+    or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,5,6)
     or os.environ.get("VAULT_TEST") == "false",
-    reason="Not supported in version. At version 0.8.5 see regresion of 8.5.6 at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md, or you have configured a false VAULT_TEST")
+    reason="0.8.5 vault: Fix [GH-4698]. 0.5.6 server/vault: [GH-2648]. Review at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md")
 def test_get_secret_from_vault_job_valid():
     url="http://localhost:8080"
     response = requests_retry_session(retries=20).get(url,timeout=60)
@@ -90,11 +102,14 @@ def test_get_secret_from_vault_job_valid():
 # De-Register Job
 # Depens on version uses versioned secrets
 #
+# NOMAD v 0.8.5 --> vault: Fix a regression in which Nomad was only compatible with Vault versions greater than 0.10.0 [GH-4698]
+# NOMAD v 0.5.6 --> server/vault: Fix Vault Client panic when given nonexistent role [GH-2648]
 @pytest.mark.skipif(tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 6, 2)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,8,5)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) <= (0,5,0)
+    or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,5,6)
     or os.environ.get("VAULT_TEST") == "false",
-    reason="Not supported in version. At version 0.8.5 see regresion of 8.5.6 at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md, or you have configured a false VAULT_TEST")
+    reason="0.8.5 vault: Fix [GH-4698]. 0.5.6 server/vault: [GH-2648]. Review at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md")
 def test_delete_vault_job(nomad_setup_vault_valid_token):
     assert "EvalID" in nomad_setup_vault_valid_token.job.deregister_job("vault")
     test_register_vault_job_valid(nomad_setup_vault_valid_token)
@@ -104,11 +119,14 @@ def test_delete_vault_job(nomad_setup_vault_valid_token):
 # Register Job with Bad Vault Token. It will report not authorized
 # Depens on version uses versioned secrets
 #
+# NOMAD v 0.8.5 --> vault: Fix a regression in which Nomad was only compatible with Vault versions greater than 0.10.0 [GH-4698]
+# NOMAD v 0.5.6 --> server/vault: Fix Vault Client panic when given nonexistent role [GH-2648]
 @pytest.mark.skipif(tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 6, 2)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,8,5)
     or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) <= (0,5,0)
+    or tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) == (0,5,6)
     or os.environ.get("VAULT_TEST") == "false",
-    reason="Not supported in version. At version 0.8.5 see regresion of 8.5.6 at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md, or you have configured a false VAULT_TEST")
+    reason="0.8.5 vault: Fix [GH-4698]. 0.5.6 server/vault: [GH-2648]. Review at https://github.com/hashicorp/nomad/blob/master/CHANGELOG.md")
 def test_register_vault_job_invalid(nomad_setup_vault_invalid_token):
     if tuple(int(i) for i in os.environ.get("VAULT_VERSION").split(".")) < (0, 9, 0):
         vault_job = "vault.json"
