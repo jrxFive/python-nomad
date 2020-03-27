44bff33b8d95f63a2dfab49fb02fa3c67f1ac540
Jonathan Cross <jrxFive@gmail.com>
2020-03-27 11:43:17 -0400

    1.2.0 release
    update development status and python version support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-nomad',
-    version='1.1.0',
+    version='1.2.0',
     install_requires=['requests'],
     packages=['nomad', 'nomad.api'],
     url='http://github.com/jrxfive/python-nomad',
@@ -11,12 +11,14 @@ setup(
     author_email='jrxfive@gmail.com',
     description='Client library for Hashicorp Nomad',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     keywords='nomad hashicorp client',
 )
