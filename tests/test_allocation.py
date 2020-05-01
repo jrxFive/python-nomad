d5788ffdcd3906c69bc166937b6873720bddc0be
Jeroen Knitel <jeroen@karb.nl>
2020-05-01 15:39:57 +0200

    Stop allocation support is present on Nomad >= 0.9.2

--- tests/test_allocation.py
+++ tests/test_allocation.py
@@ -3,6 +3,7 @@ import json
 import uuid
 import responses
 import tests.common as common
+import os
 
 
 # integration tests requires nomad Vagrant VM or Binary running
@@ -18,7 +19,7 @@ def test_get_allocation(nomad_setup):
     id = nomad_setup.job.get_allocations("example")[0]["ID"]
     assert isinstance(nomad_setup.allocation.get_allocation(id), dict) == True
 
-
+@pytest.mark.skipif(tuple(int(i) for i in os.environ.get("NOMAD_VERSION").split(".")) < (0, 9, 2), reason="Nomad alloc stop not supported")
 def test_stop_allocation(nomad_setup):
     id = nomad_setup.job.get_allocations("example")[0]["ID"]
     assert isinstance(nomad_setup.allocation.stop_allocation(id), dict) == True
