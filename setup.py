23c21c563ca2a6f85795f49207430503e0ffe3c9
Nikita Beletskii <nikita.beletskii@behavox.com>
2022-09-05 16:45:34 +0200

    change requests lib version

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
