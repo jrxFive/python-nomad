490b1489f698dd7860804c77b13fe1b6a4bafee9
Nikita Beletskii <2nikita.b@gmail.com>
2022-12-06 16:16:25 +0100

    Remove support for Python 2, Python 3.6 and upgrade deps (#142)

--- setup.py
+++ setup.py
@@ -19,8 +19,6 @@ setuptools.setup(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
